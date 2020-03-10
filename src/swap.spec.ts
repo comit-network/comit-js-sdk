f1d7e46ec475b41884283cac3b92962239a0e99d
Franck Royer <franck@coblox.tech>
2020-03-11 10:50:48 +1100

    Throw ChainError when executing an action if the wallet throws

--- src/swap.spec.ts
+++ src/swap.spec.ts
@@ -1,6 +1,6 @@
 import nock = require("nock");
 import { Problem } from "./cnd/axios_rfc7807_middleware";
-import { Cnd, SwapDetails } from "./cnd/cnd";
+import { Cnd, LedgerAction, SwapDetails } from "./cnd/cnd";
 import { ChainError, Swap } from "./swap";
 
 describe("Swap", () => {
@@ -38,7 +38,7 @@ describe("Swap", () => {
     return scope;
   });
 
-  it("Throws a ChainError if a wallet returns an error when executing an action", async () => {
+  it("Throws a ChainError if a wallet returns an error when resolving fields", async () => {
     const basePath = "http://example.com";
     const cnd = new Cnd(basePath);
 
@@ -69,5 +69,62 @@ describe("Swap", () => {
     await expect(promise).rejects.toBeInstanceOf(ChainError);
 
     return scope;
+  });
+
+  it("Throws a ChainError if a wallet returns an error when executing an action", async () => {
+    const basePath = "http://example.com";
+    const cnd = new Cnd(basePath);
+
+    const selfPath = "/swap-id";
+
+    const swap = new Swap(cnd, `${basePath}/swap-id`, {});
+
+    const swapDetails: SwapDetails = {
+      actions: [
+        {
+          href: "/swap-id/deploy",
+          name: "deploy",
+          method: "GET"
+        }
+      ]
+    };
+
+    const swapScope = nock(basePath)
+      .get(selfPath)
+      .times(2)
+      .reply(200, JSON.stringify(swapDetails), {});
+
+    const actionPayload = {
+      type: "ethereum-deploy-contract",
+      payload: {
+        data: "",
+        amount: "",
+        gas_limit: "",
+        network: ""
+      }
+    };
+
+    const actionScope = nock(basePath)
+      .get("/swap-id/deploy")
+      .times(2)
+      .reply(200, JSON.stringify(actionPayload), {});
+
+    const tryParams = {
+      maxTimeoutSecs: 1,
+      tryIntervalSecs: 0.01
+    };
+
+    // To be sure we are testing the right thing, we verify this does not throws
+    const executeActionPromise = swap.tryExecuteSirenAction<LedgerAction>(
+      "deploy",
+      tryParams
+    );
+    await expect(executeActionPromise).resolves;
+
+    const promise = swap.deploy(tryParams);
+
+    await expect(promise).rejects.toBeInstanceOf(ChainError);
+
+    return { swapScope, actionScope };
   });
 });
