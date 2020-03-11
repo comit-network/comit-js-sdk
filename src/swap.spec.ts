79b498cfacb6628ac6119268b3a9b9bd3b050b4a
Franck Royer <franck@coblox.tech>
2020-03-11 14:30:53 +1100

    Replace nock with cnd method mocking

--- src/swap.spec.ts
+++ src/swap.spec.ts
@@ -1,6 +1,5 @@
-import nock = require("nock");
 import { Problem } from "./cnd/axios_rfc7807_middleware";
-import { Cnd, LedgerAction, SwapDetails } from "./cnd/cnd";
+import { Cnd } from "./cnd/cnd";
 import { Swap, WalletError } from "./swap";
 
 describe("Swap", () => {
@@ -12,21 +11,9 @@ describe("Swap", () => {
 
     const swap = new Swap(cnd, selfPath, {});
 
-    nock(basePath)
-      .get(selfPath)
-      .reply(
-        400,
-        JSON.stringify({
-          status: "400",
-          title: "Swap not supported",
-          type: "about:blank",
-          detail:
-            "The requested combination of ledgers and assets is not supported."
-        }),
-        {
-          "content-type": "application/problem+json"
-        }
-      );
+    cnd.fetch = jest.fn(async () => {
+      throw new Problem({ title: "" });
+    });
 
     const promise = swap.tryExecuteSirenAction("action", {
       maxTimeoutSecs: 1,
@@ -40,24 +27,23 @@ describe("Swap", () => {
     const basePath = "http://example.com";
     const cnd = new Cnd(basePath);
 
-    const selfPath = "/swap-id";
-
     const swap = new Swap(cnd, `${basePath}/swap-id`, {});
 
-    const swapDetails: SwapDetails = {
-      actions: [
-        {
-          href: "/swap-id/action",
-          name: "action",
-          method: "GET",
-          fields: [{ name: "address", class: ["bitcoin", "address"] }]
+    // @ts-ignore: This should return an AxiosPromise
+    cnd.fetch = jest.fn(async () => {
+      return {
+        data: {
+          actions: [
+            {
+              href: "/swap-id/action",
+              name: "action",
+              method: "GET",
+              fields: [{ name: "address", class: ["bitcoin", "address"] }]
+            }
+          ]
         }
-      ]
-    };
-
-    nock(basePath)
-      .get(selfPath)
-      .reply(200, JSON.stringify(swapDetails), {});
+      };
+    });
 
     const promise = swap.tryExecuteSirenAction("action", {
       maxTimeoutSecs: 1,
@@ -71,51 +57,42 @@ describe("Swap", () => {
     const basePath = "http://example.com";
     const cnd = new Cnd(basePath);
 
-    const selfPath = "/swap-id";
-
     const swap = new Swap(cnd, `${basePath}/swap-id`, {});
 
-    const swapDetails: SwapDetails = {
-      actions: [
-        {
-          href: "/swap-id/deploy",
-          name: "deploy",
-          method: "GET"
+    // @ts-ignore: This should return an AxiosPromise
+    cnd.fetch = jest.fn(async () => {
+      return {
+        data: {
+          actions: [
+            {
+              href: "/swap-id/deploy",
+              name: "deploy",
+              method: "GET"
+            }
+          ]
         }
-      ]
-    };
+      };
+    });
 
-    nock(basePath)
-      .get(selfPath)
-      .times(2)
-      .reply(200, JSON.stringify(swapDetails), {});
-
-    const actionPayload = {
-      type: "ethereum-deploy-contract",
-      payload: {
-        data: "",
-        amount: "",
-        gas_limit: "",
-        network: ""
-      }
-    };
-
-    nock(basePath)
-      .get("/swap-id/deploy")
-      .times(2)
-      .reply(200, JSON.stringify(actionPayload), {});
+    // @ts-ignore: This should return an AxiosPromise
+    cnd.executeSirenAction = jest.fn(async () => {
+      return {
+        data: {
+          type: "ethereum-deploy-contract",
+          payload: {
+            data: "",
+            amount: "",
+            gas_limit: "",
+            network: ""
+          }
+        }
+      };
+    });
 
     const tryParams = {
       maxTimeoutSecs: 1,
       tryIntervalSecs: 0.01
     };
-
-    // To be sure we are testing the right thing, we verify this does not throw
-    const executeActionPromise = swap.tryExecuteSirenAction<LedgerAction>(
-      "deploy",
-      tryParams
-    );
-    await expect(executeActionPromise).resolves;
 
     const promise = swap.deploy(tryParams);
 
--- src/swap.ts
+++ src/swap.ts
@@ -11,7 +11,7 @@ export class WalletError extends Error {
     public readonly source: Error,
     public readonly callParams: any
   ) {
-    super(source.message);
+    super(JSON.stringify(source));
   }
 }
 
