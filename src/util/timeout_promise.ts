b81982d5a0c095bc08bd824e3f04123a085ef791
Lucas Soriano del Pino <l.soriano.del.pino@gmail.com>
2020-03-28 11:43:21 +1100

    Clear timeout even if race resolves before timeout
    Otherwise the handle remains open until the timeout promise resolves,
    which can make certain programs hang unexpectedly.

--- src/util/timeout_promise.ts
+++ src/util/timeout_promise.ts
@@ -10,14 +10,19 @@ export async function timeoutPromise<T>(
   ms: number,
   promise: Promise<T>
 ): Promise<T> {
+  let id: NodeJS.Timeout;
+
+  // Create a promise that rejects in <ms> milliseconds
   const timeout = new Promise<T>((_, reject) => {
-    const id = setTimeout(() => {
-      clearTimeout(id);
-      reject(`Timed out in ${ms}ms.`);
+    id = setTimeout(() => {
+      reject(new Error(`timed out after ${ms}ms`));
     }, ms);
   });
 
-  return Promise.race([promise, timeout]);
+  // Returns a race between our timeout and the passed in promise
+  return Promise.race([promise, timeout]).finally(() => {
+    clearTimeout(id);
+  });
 }
 
 export async function sleep(ms: number): Promise<void> {
